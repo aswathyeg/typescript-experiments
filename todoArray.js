504ac132202f577cadc8857c106062248e87f1f7
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-03-25 23:49:13 +0530

    new component

---
+++ todoArray.js
@@ -0,0 +1,7 @@
+function address1(todose) {
+    return {
+        id: 1,
+        name: "jay",
+        place: "fi"
+    };
+}
---
+++ todoArray.ts
@@ -0,0 +1,14 @@
+type Todo={
+    id:number,
+    name:string,
+    place:string
+}
+function address1(todose:Todo[]){
+    return{
+    id:1,
+    name:"jay",
+    place:"fi"
+}
+
+}
+console.log (address1.name)
