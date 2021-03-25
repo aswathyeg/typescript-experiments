4a4ed9e0596d1ca2be9ba9b7209efb5010d1ba0a
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-03-25 16:14:51 +0530

    type example

---
+++ todo.ts
@@ -0,0 +1,13 @@
+type Details={
+    id:number,
+    name:string,
+    phone:number
+
+}
+function address(details:Details){
+    return{
+        id:1,
+        name:"sindhu",
+        phone:1222
+    }
+}
