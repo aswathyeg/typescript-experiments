831a0167d4d4f006c1878c9eaf146b1afb43ee56
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-03-26 23:25:41 +0530

    function with arguments

---
+++ funExample.js
@@ -0,0 +1,4 @@
+function greet(name, place, id) {
+    console.log("Name:" + name + "Place" + place + "Id" + id);
+}
+greet("jenny", "Cern", 1);
---
+++ funExample.ts
@@ -0,0 +1,5 @@
+function greet(name:string,place:string,id:number){
+    console.log("Name:"+name+"Place:"+place+"Id:"+id);
+
+}
+greet("jenny","Cern",1);
