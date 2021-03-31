e6e671ee8273e0e9ab0e1f953363dae44562a7f4
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-04-01 00:12:52 +0530

    function generic type example

---
+++ fnGenerictype.ts
@@ -0,0 +1,10 @@
+function firstElement<Type>(arr:Type[]):Type{
+    return arr[0]
+
+}
+// s is of type 'string'
+const s = firstElement(["a", "b", "c"]);
+// n is of type 'number'
+const n = firstElement([1, 2, 3]);
+console.log(s);
+console.log(n[1]);
