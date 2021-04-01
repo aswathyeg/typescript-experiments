ec2914204dc3c4e958aa3f7e4a80acab3dee94b4
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-04-01 11:50:37 +0530

    comment added

--- funConstrains.ts
+++ funConstrains.ts
@@ -7,5 +7,6 @@ return b;
 }
 const longerArray=longest([1,2],[1,2,3,4]);
 const longerString=longest("aeriobics","aero");
+//const longerNumber=longest(50,12); this cause error bcz in(50,12) .length property is not applicable
 console.log(longerArray);
 console.log(longerString);
