cc7d737476c81e46067f4f661ca17b7c16aaeac6
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-04-09 19:16:18 +0530

    simple fn multiplication

---
+++ multiply.js
@@ -0,0 +1,6 @@
+function multiply(e, f) {
+    return e * f;
+}
+var e = 2;
+var f = 3;
+console.log(e + "*" + f + "=" + multiply(e, f));
---
+++ multiply.ts
@@ -0,0 +1,8 @@
+
+
+function multiply(e:number,f:number): number{
+    return e*f;
+}
+const e=2;
+const f=3;
+console.log(`${e}*${f}=${multiply(e,f)}`)
