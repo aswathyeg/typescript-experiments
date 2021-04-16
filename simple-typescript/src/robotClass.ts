04dcaac8e18b5d23661bc5ee74f64d1fe38b5768
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-04-16 11:39:59 +0530

    added child class

--- simple-typescript/src/robotClass.ts
+++ simple-typescript/src/robotClass.ts
@@ -12,5 +12,22 @@ class Robot{
          
      }
 }
+
+class FlyingRobot extends Robot{
+    jetPacksize:number;
+    constructor(name:string,jetPacksize:number){
+        super(name);
+        this.jetPacksize=jetPacksize;
+       
+    }
+    move(distance:number){
+        console.log(`${this.name} is flying`);
+        super.move(distance);
+
+    }
+}
 const robot=new Robot('John');
 robot.askMe();
+
+const flyingrbot=new FlyingRobot('Jim',2)
+flyingrbot.move(10);
