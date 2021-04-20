c8fa3b1f8e0266b4a4f4a9275092ea20e4e0f025
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-04-20 18:41:19 +0530

    example to classes and interface

---
+++ simple-typescript/src/animalInterface.ts
@@ -0,0 +1,16 @@
+interface Animal{
+    name:string;
+}
+class Cat implements Animal{
+    name:string;
+    constructor(name:string){
+         this.name=name;
+    }
+}
+class Dog implements Animal{
+    name:string;
+    constructor (name:string){
+        this.name=name;
+    }
+}
+
