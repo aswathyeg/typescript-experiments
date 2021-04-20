5249eefbb1b5fa2273438d314bc42ef940bac744
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-04-20 19:45:28 +0530

    added 2 more properties to interface

--- simple-typescript/src/animalInterface.ts
+++ simple-typescript/src/animalInterface.ts
@@ -1,16 +1,34 @@
 interface Animal{
     name:string;
+    group:string|undefined;
+    setGroup(group:string):void
 }
 class Cat implements Animal{
     name:string;
+    group:string|undefined;
     constructor(name:string){
          this.name=name;
+    }
+    setGroup(group:string){
+        this.group=group;
     }
 }
 class Dog implements Animal{
     name:string;
+    group:string|undefined;
+
     constructor (name:string){
         this.name=name;
     }
+    setGroup(group:string){
+        this.group=group
+    }
+}
+interface AnimalConstructor{
+    new (name:string):Animal // constructor signature
 }
 
+function initializeAnimal(Animal:AnimalConstructor,name:string) {
+    const animal=new Animal(name);
+    
+}
