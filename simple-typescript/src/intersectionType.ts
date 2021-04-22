87fcff4383f18739c106206761c3ffdf56cad094
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-04-22 13:45:10 +0530

    formatted

--- simple-typescript/src/intersectionType.ts
+++ simple-typescript/src/intersectionType.ts
@@ -1,22 +1,22 @@
-interface IA{
-    a:number;
+interface IA {
+    a: number;
 }
-interface IB{
-    b:number;
+interface IB {
+    b: number;
 }
-interface IC{
-    c:number;
+interface IC {
+    c: number;
 }
-function x(obj:IA & IB & IC){
-return obj.a +obj.b + obj.c;
+function x(obj: IA & IB & IC) {
+    return obj.a + obj.b + obj.c;
 
 }
-function combine<ObjA,ObjB>(objA:ObjA,objB:ObjB):ObjA & ObjB{//ObjA & ObjB are parameters to describe 
+function combine<ObjA, ObjB>(objA: ObjA, objB: ObjB): ObjA & ObjB {//ObjA & ObjB are parameters to describe 
     //type of objects .return type to avoid logic error
-    return {...objA,...objB}
+    return { ...objA, ...objB }
 
 }
-const objA={a:1}
-const objB={b:2}
-const resultObj=combine(objA,objB);
+const objA = { a: 1 }
+const objB = { b: 2 }
+const resultObj = combine(objA, objB);
 console.log(resultObj);
