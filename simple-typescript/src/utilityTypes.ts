c33b0d3453d8955759c9a0445334297c5c66c7d5
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-04-23 20:31:26 +0530

    trying to update only one property from the interface

---
+++ simple-typescript/src/utilityTypes.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+interface Starship{
+    name:string;
+    enableJump:boolean;
+}
+const updateStarship=(id:number,starship:Starship)=>{
+
+}
+updateStarship({1,'ShipAnna'});
+
