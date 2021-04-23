8e4b4bcfa27678bbbe58a210d12e79db6b0cebed
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-04-23 20:45:22 +0530

    added partial

--- simple-typescript/src/utilityTypes.ts
+++ simple-typescript/src/utilityTypes.ts
@@ -3,8 +3,10 @@ interface Starship{
     name:string;
     enableJump:boolean;
 }
-const updateStarship=(id:number,starship:Starship)=>{
+const updateStarship=(id:number,starship:Partial<Starship>)=>{
+    //partial makes to call only required properties of function
 
 }
-updateStarship(1,{'ShipAnna'});
+updateStarship(1,{
+    name:'ShipAnna'});
 
