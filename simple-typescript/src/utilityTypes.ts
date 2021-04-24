138abbd3deed3a26ec8be9cbf90259654e68d338
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-04-24 16:45:42 +0530

    added Pick type

--- simple-typescript/src/utilityTypes.ts
+++ simple-typescript/src/utilityTypes.ts
@@ -2,6 +2,7 @@ import React from 'react';
 interface Starship{
     name:string;
     enableJump:boolean;
+    idNumber:number;
 }
 const updateStarship=(id:number,starship:Partial<Starship>)=>{
     //partial makes to call only required properties of function
@@ -9,4 +10,8 @@ const updateStarship=(id:number,starship:Partial<Starship>)=>{
 }
 updateStarship(1,{
     name:'ShipAnna'});
+
+    type pickingProps=Pick<Starship,'name'>;//Pick eg
+
+    
 
