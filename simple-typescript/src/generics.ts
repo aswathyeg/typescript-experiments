f56752f9ab3e707b17a4e943c2ddcf81c5e97c44
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-04-28 16:29:57 +0530

    added eg code to more generic types

--- simple-typescript/src/generics.ts
+++ simple-typescript/src/generics.ts
@@ -3,3 +3,24 @@
 function genericFunction<T>(x:T):T {
     return x;
 }
+//arrow fn eg
+const genericArrowFunction=<T>(x:T):T=>x;
+
+//generic interface
+interface GenericInterface<T>{
+(a:T):T;
+someProp:T;
+}
+
+interface GenericInterface2<T>{
+<U>(a:U):U
+someProp:T;
+}
+//Generic classes
+class GenericClass<P>{
+constructor(public props:P){}
+    
+getProps():P{
+  return this.props
+}
+}
