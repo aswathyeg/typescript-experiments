9ec8a0c8fab8820f7ad0b16b7acc080430f95a87
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-05-03 22:17:45 +0530

    added nodemodules and dist

--- react-plus-typescript/webpack.config.js
+++ react-plus-typescript/webpack.config.js
@@ -1,8 +1,8 @@
-const htmlWebpackPlugin=require('html-webpack-plugin')
+const HtmlWebpackPlugin=require('html-webpack-plugin')
 const { Module } = require('html-webpack-plugin')
 module.exports={
     plugins:[
-        new htmlWebpackPlugin({
+        new HtmlWebpackPlugin({
             template:'./src/index.html',
             filename:'index.html',
         }),
