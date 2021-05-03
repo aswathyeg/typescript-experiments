79bd0838f0b040ce75e03d281bd6cffe6546f861
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-05-03 23:13:41 +0530

    updated

--- react-plus-typescript/webpack.config.js
+++ react-plus-typescript/webpack.config.js
@@ -1,10 +1,10 @@
-const HtmlWebpackPlugin=require('html-webpack-plugin')
-const { Module } = require('html-webpack-plugin')
-module.exports={
-    plugins:[
-        new HtmlWebpackPlugin({
-            template:'./src/index.html',
-            filename:'index.html',
-        }),
-    ],
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
 };
