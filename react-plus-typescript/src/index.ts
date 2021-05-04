9adbb03669951a79afc453c33e7e55b0a6eb9504
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-05-05 00:03:53 +0530

    updated

--- react-plus-typescript/src/index.ts
+++ react-plus-typescript/src/index.ts
@@ -1,11 +1,5 @@
-export function log(str: string) {
-    console.log(str);
-  }
-  
-  log('Hello World');
-  
-  class A {
-    greeting = 'Hello World';
-  }
-  
-  console.log(new A().greeting);
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App=()=>{
+  return <div> Hello </div>;
+};
