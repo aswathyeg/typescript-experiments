f3eb030a0cf22e77845150701f3a65458937d31b
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-05-08 00:03:21 +0530

    styles added

--- react-plus-typescript/src/components/App.tsx
+++ react-plus-typescript/src/components/App.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import Student from './student';
 import students from '../data/students.json';
 import AppCSS from './App.module.css';
+import Group from './Group';
 //import StudentSVG from '../svg/student.svg';
 
 const App=()=>{ //function component
     return (
         <div className={AppCSS.container}>
+            <div className={AppCSS.header}>
+<div className={AppCSS.setTitle}>Students</div>
+<Group />
+            </div>
     <ul>
        {students.map((student)=>{
            return<Student student={student}/>
