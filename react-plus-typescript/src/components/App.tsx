82546c6115f6c1100c8a636c267aee993cbe12d8
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-05-07 14:47:31 +0530

    imported css file

--- react-plus-typescript/src/components/App.tsx
+++ react-plus-typescript/src/components/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Student from './student';
 import students from '../data/students.json';
+import AppCSs from './App.module.css';
 
 const App=()=>{ //function component
     return (
+        <div>
     <ul>
        {students.map((student)=>{
            return<Student student={student}/>
@@ -11,6 +13,6 @@ const App=()=>{ //function component
        })
 }
 
-    </ul>)
+    </ul></div>)
 }
 export default App;
