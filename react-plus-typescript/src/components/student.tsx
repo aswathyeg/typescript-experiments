69a0356f3f6d7168840822b7bce661333c2a5a32
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-05-12 18:37:33 +0530

    set state configured and added add button

--- react-plus-typescript/src/components/student.tsx
+++ react-plus-typescript/src/components/student.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import StudentCSS from './student.module.css';
+import {AppSetStateContext} from './AppState';
+
 interface Student{
 id:number,
 name:string,
@@ -9,11 +11,16 @@ interface Props{
   student : Student;
 }
 const Student:React.FC<Props>=({student})=>{
+  const setState=useContext(AppSetStateContext);
+  const onClickAdd=()=>{
+    
+  }
     return <div>
       <li className={StudentCSS.container}>
         <h2>{student.name}</h2>
         <p>{student.id}</p>
         <p>{student.phone}</p>
+        <button type="button" onClick={onClickAdd}>Add</button>
         
         </li></div>
 
