26e603293f3e0916e559e5ac6caada41eda12311
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-05-13 11:49:06 +0530

    added setState function

--- react-plus-typescript/src/components/student.tsx
+++ react-plus-typescript/src/components/student.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import StudentCSS from './student.module.css';
-import {AppSetStateContext} from './AppState';
-import {useSetState} from './AppState'
+import {useSetState} from './AppState';
+
 interface Student{
 id:number,
 name:string,
@@ -11,11 +11,18 @@ interface Props{
   student : Student;
 }
 const Student:React.FC<Props>=({student})=>{
-  const setState=useSetState();
-  const onClickAdd=()=>{
-    
-  }
-    return <div>
+  const setState = useSetState();
+  const onClickAdd = ()=>{
+   setState((state)=>{
+    const itemExists = state.group.students.find((student) => student.id === student.id);
+     return{
+       ...state,
+       group:{...state.group,students:
+      [...state.group.students,{id:student.id,name:student.name}]}};//all the state properties that dont want to update
+   });
+  };
+    return (
+    <div>
       <li className={StudentCSS.container}>
         <h2>{student.name}</h2>
         <p>{student.id}</p>
@@ -23,6 +30,7 @@ const Student:React.FC<Props>=({student})=>{
         <button type="button" onClick={onClickAdd}>Add</button>
         
         </li></div>
+    )
 
 };
 export default Student;
