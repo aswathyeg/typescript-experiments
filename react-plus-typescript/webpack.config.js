8f4e031f177cd23b9374a9349f0b565049e03e70
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-05-14 16:59:57 +0530

    updated

--- react-plus-typescript/webpack.config.js
+++ react-plus-typescript/webpack.config.js
@@ -1,9 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin=require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
-  devtool:"eval-source-map",
+  devtool: 'eval-source-map',
   resolve: {
     extensions: ['.js', '.ts', '.tsx'],
   },
@@ -15,26 +15,30 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-test:/\.css$/,
-use:[MiniCssExtractPlugin.loader,
-  {loader:'css-loader',options:{modules:true}},],
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { modules: true } },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        loader: '@svgr/webpack',
+        options: {
+          svgoConfig: {
+            plugins: {
+              removeViewBox: false,
+            },
+          },
+        },
       },
     ],
   },
-  
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
     }),
     new MiniCssExtractPlugin(),
-
-    
   ],
-},
-{
-  test:/\.svg$/,
-  loader:'@svgr/webpack',
-}
-
-;
+};
