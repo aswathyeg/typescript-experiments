edd395dfab7e213be13fbcdd203a1db6fec524d5
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-05-23 23:10:26 +0530

    added dispatch function

--- react-plus-typescript/src/components/Add.tsx
+++ react-plus-typescript/src/components/Add.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { StudentDetails, useStateDispatch } from './AppState';
+import student from './student';
 function withAdd<OriginalProps>(ChildComponent:React.ComponentType<OriginalProps>){
 const AddHOC=(props:OriginalProps)=>{
+    const dispatch = useStateDispatch();
+  const onClickAdd = (item:StudentDetails)=>{
+    dispatch({
+      type: 'Add_To_Group',
+      payload: {
+        item,
+      },
+    });
+};
 return <ChildComponent {...props} />
 }
 return AddHOC;
