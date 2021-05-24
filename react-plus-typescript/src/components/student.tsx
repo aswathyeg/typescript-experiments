1f185d578fb80f06969ad14eaac14067c38d1bd3
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-05-24 16:27:08 +0530

    formated

--- react-plus-typescript/src/components/student.tsx
+++ react-plus-typescript/src/components/student.tsx
@@ -1,31 +1,30 @@
 import React from 'react';
 import StudentCSS from './student.module.css';
-import {useStateDispatch} from './AppState';
-import {Student} from '../types';
+import { Student } from '../types';
 import { AddToGroupProps, withAdd } from './Add';
 
 
-interface Props extends AddToGroupProps{
-  student : Student;
+interface Props extends AddToGroupProps {
+  student: Student;
 }
-const StudentItem:React.FC<Props>=({student,addToGroup})=>{
-  
-  const onClickAdd = ()=>{
+const StudentItem: React.FC<Props> = ({ student, addToGroup }) => {
 
-    addToGroup({id:student.id,name:student.name,phone:student.phone})
+  const onClickAdd = () => {
+
+    addToGroup({ id: student.id, name: student.name, phone: student.phone })
 
   }
-    
-    return (
+
+  return (
     <div>
       <li className={StudentCSS.container}>
         <h2>{student.name}</h2>
         <p>{student.id}</p>
         <p>{student.phone}</p>
         <button type="button" onClick={onClickAdd}>Add</button>
-        
-        </li></div>
-    )
 
-    }
-export default withAdd (StudentItem);
+      </li></div>
+  )
+
+}
+export default withAdd(StudentItem);
